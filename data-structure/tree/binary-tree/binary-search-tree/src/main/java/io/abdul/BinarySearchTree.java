7760676923e96e831688e675fd3a6d933942cdc2
Mohideen Abdul Katheer M <abdulkatheerm@gmail.com>
2024-05-01 10:21:33 +0530

    Implements BinarySearchTree remove in Iterative style

--- data-structure/tree/binary-tree/binary-search-tree/src/main/java/io/abdul/BinarySearchTree.java
+++ data-structure/tree/binary-tree/binary-search-tree/src/main/java/io/abdul/BinarySearchTree.java
@@ -1,5 +1,7 @@
 package io.abdul;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -68,33 +70,33 @@ public class BinarySearchTree<E extends Comparable<E>> {
     }
 
     public boolean remove(E element) {
-        Node<E> parent = null;
-        Node<E> current = root;
-        boolean currentLeftNode = false;
-        boolean removed = false;
+        if (root == null) {
+            return false;
+        }
 
-        while (current != null) {
-            int compare = element.compareTo(current.value);
-            if (compare == 0) { // match found
-                if (current.isLeaf()) { // lead node
-                    removeLeafNode(parent, currentLeftNode);
-                } else if (current.hasOneChild()) { // only one child
-                    removeNodeWithOneChild(current, parent, currentLeftNode);
-                } else { // two children
-                    removeNodeWithTwoChildren(current, parent, currentLeftNode);
+        if (element.compareTo(root.value) == 0) { // removing root element
+            // No leaf
+            // 1 child
+            // 2 children
+            if (root.isLeaf()) { // lead node
+                root = null;
+            } else if (root.hasOneChild()) { // only one child
+                Node<E> grandChild;
+                if (root.left != null) {
+                    grandChild = root.left;
+                } else {
+                    grandChild = root.right;
                 }
-                removed = true;
-                break;
-            } else {
-                parent = current;
-                if (compare < 0) { // Traverse left
-                    current = current.left;
-                    currentLeftNode = true;
-                } else { // Traverse right
-                    current = current.right;
-                    currentLeftNode = false;
-                }
+                root = grandChild;
+            } else { // two children
+                removeNodeWithTwoChildren(root);
             }
+            size--;
+            return true;
+        }
+        boolean removed = removeElement(root, element);
+        if (removed) {
+            size--;
         }
         return removed;
     }
@@ -114,10 +116,23 @@ public class BinarySearchTree<E extends Comparable<E>> {
         private Node<E> left;
         private Node<E> right;
 
+        public E getValue() {
+            return value;
+        }
+
+        public Node<E> getLeft() {
+            return left;
+        }
+
+        public Node<E> getRight() {
+            return right;
+        }
+
         public Node(E value) {
             this.value = value;
         }
 
+        @JsonIgnore
         public boolean isLeaf() {
             return left == null && right == null;
         }
@@ -136,45 +151,88 @@ public class BinarySearchTree<E extends Comparable<E>> {
         inOrderTraversal(node.right, elements);
     }
 
-    private void removeNodeWithTwoChildren(Node<E> current, Node<E> parent, boolean currentLeftNode) {
-        Node<E> successor = current.right;
-        Node<E> predecessor = current.left;
-        successor.left = predecessor; // making left node pointing to right node
-        if (parent == null) { // deleting root node and it has two children, making the successor child as root
-            root = successor;
-        } else if (currentLeftNode) {
-            parent.left = successor;
-        } else {
-            parent.right = successor;
+    private static <E extends Comparable<E>> boolean removeElement(Node<E> root, E element) {
+        int comp = element.compareTo(root.value);
+        Node<E> parent = root;
+        Node<E> current;
+        boolean currentLeftNode;
+        boolean removed = false;
+        if (comp < 0) { // Traverse left
+            current = parent.left;
+            currentLeftNode = true;
+        } else { // Traverse right
+            current = parent.right;
+            currentLeftNode = false;
         }
-        size--;
+
+        while (current != null) {
+            int compare = element.compareTo(current.value);
+            if (compare == 0) { // match found
+                if (current.isLeaf()) { // lead node
+                    removeLeafNode(parent, currentLeftNode);
+                } else if (current.hasOneChild()) { // only one child
+                    removeNodeWithOneChild(current, parent, currentLeftNode);
+                } else { // two children
+                    removeNodeWithTwoChildren(current);
+                }
+                removed = true;
+                break;
+            } else {
+                parent = current;
+                if (compare < 0) { // Traverse left
+                    current = current.left;
+                    currentLeftNode = true;
+                } else { // Traverse right
+                    current = current.right;
+                    currentLeftNode = false;
+                }
+            }
+        }
+        return removed;
     }
 
-    private void removeNodeWithOneChild(Node<E> current, Node<E> parent, boolean currentLeftNode) {
+    private static <E extends Comparable<E>> void removeNodeWithTwoChildren(Node<E> nodeToBeRemoved) {
+        // find successor of current
+        // copy value of successor to current
+        // remove successor (no  child or 1-child case)
+        Node<E> successor = findSuccessor(nodeToBeRemoved);
+        nodeToBeRemoved.value = successor.value;
+        removeElement(nodeToBeRemoved, successor.value);
+    }
+
+    private static <E extends Comparable<E>> Node<E> findSuccessor(Node<E> parent) {
+        Node<E> successor = parent.right;
+        Node<E> current = successor.left;
+
+        while (current != null) {
+            if (current.left != null && current.left.value.compareTo(successor.value) < 0) {
+                successor = current.left;
+            }
+            current = current.left;
+        }
+
+        return successor;
+    }
+
+    private static <E> void removeNodeWithOneChild(Node<E> current, Node<E> parent, boolean currentLeftNode) {
         Node<E> grandChild;
         if (current.left != null) {
             grandChild = current.left;
         } else {
             grandChild = current.right;
         }
-        if (parent == null) { // deleting root node and it has one child, making the grand child as root // done
-            root = grandChild;
-        } else if (currentLeftNode) { // done
+        if (currentLeftNode) { // done
             parent.left = grandChild; // Replacing current with its grandchild
         } else { // done
             parent.right = grandChild;
         }
-        size--;
     }
 
-    private void removeLeafNode(Node<E> parent, boolean currentLeftNode) {
-        if (parent == null) { // deleting root node and it's leaf node // done
-            root = null;
-        } else if (currentLeftNode) { // done
+    private static <E> void removeLeafNode(Node<E> parent, boolean currentLeftNode) {
+        if (currentLeftNode) { // done
             parent.left = null;
         } else { // done
             parent.right = null;
         }
-        size--;
     }
 }
--- data-structure/tree/binary-tree/binary-search-tree/src/test/java/io/abdul/BinarySearchTreeTest.java
+++ data-structure/tree/binary-tree/binary-search-tree/src/test/java/io/abdul/BinarySearchTreeTest.java
@@ -1,12 +1,20 @@
 package io.abdul;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Field;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BinarySearchTreeTest {
+    ClassLoader classLoader = getClass().getClassLoader();
+    ObjectMapper objectMapper = new ObjectMapper();
 
     @Test
     void insert() {
@@ -40,7 +48,7 @@ class BinarySearchTreeTest {
     }
 
     @Test
-    void remove() {
+    void remove() throws NoSuchFieldException, IllegalAccessException, IOException {
         BinarySearchTree<String> stringTree = new BinarySearchTree<>();
 
         testEmptyTree(stringTree);
@@ -64,17 +72,107 @@ class BinarySearchTreeTest {
         testRemovingRightParentWithTwoChildren(stringTree);
 
         testRemovingLeftParentWithTwoChildren(stringTree);
+
+        testRemovingNonRootNodeWithTwoChildrenAndSuccessorIsImmediateRightAndSuccessorIsHavingOneChild();
+
+        testRemovingNonRootNodeWithTwoChildrenAndSuccessorIsFarFromNodeAndSuccessorIsHavingOneChild();
+
+        testRemovingNonRootNodeWithTwoChildrenAndSuccessorFarFromNodeAndSuccessorIsHavingNoChild();
     }
 
-    private static void testRemovingRightParentWithTwoChildren(BinarySearchTree<String> stringTree) {
+    private void testRemovingNonRootNodeWithTwoChildrenAndSuccessorIsFarFromNodeAndSuccessorIsHavingOneChild() throws NoSuchFieldException, IllegalAccessException, IOException {
+        BinarySearchTree<Integer> numbers = new BinarySearchTree<>();
+        numbers.insert(100);
+        numbers.insert(9);
+        numbers.insert(105);
+        numbers.insert(7);
+        numbers.insert(40);
+        numbers.insert(30);
+        numbers.insert(10);
+        numbers.insert(15);
+        numbers.insert(20);
+
+        removeAndTest(numbers, "remove/case1_input.json", "remove/case1_output.json", 9, 8);
+    }
+
+    private <E extends Comparable<E>> void removeAndTest(BinarySearchTree<E> numbers, String inputFilePath,
+                                                         String outputFilePath, E elementToBeRemoved, int expectedSize)
+            throws NoSuchFieldException, IllegalAccessException, IOException {
+        BinarySearchTree.Node<E> rootNode;
+        rootNode = getRootNode(numbers);
+
+        String input = objectMapper.writeValueAsString(rootNode);
+        try (InputStream is = new FileInputStream(classLoader.getResource(inputFilePath).getFile())) {
+            byte[] expectedInputTree = is.readAllBytes();
+            assertEquals(objectMapper.readTree(expectedInputTree), objectMapper.readTree(input));
+        }
+
+        assertTrue(numbers.remove(elementToBeRemoved));
+
+        rootNode = getRootNode(numbers);
+        String output = objectMapper.writeValueAsString(rootNode);
+        if (outputFilePath == null) {
+            assertEquals("null", output);
+        } else {
+            try (InputStream is = new FileInputStream(classLoader.getResource(outputFilePath).getFile())) {
+                byte[] expectedOutputTree = is.readAllBytes();
+                assertEquals(objectMapper.readTree(expectedOutputTree), objectMapper.readTree(output));
+            }
+        }
+        assertEquals(expectedSize, numbers.size());
+    }
+
+    private void testRemovingNonRootNodeWithTwoChildrenAndSuccessorFarFromNodeAndSuccessorIsHavingNoChild() throws IOException, NoSuchFieldException, IllegalAccessException {
+        BinarySearchTree<Integer> numbers = new BinarySearchTree<>();
+        numbers.insert(12);
+        numbers.insert(11);
+        numbers.insert(1);
+        numbers.insert(0);
+        numbers.insert(6);
+        numbers.insert(5);
+        numbers.insert(7);
+        numbers.insert(4);
+        numbers.insert(8);
+        numbers.insert(3);
+        numbers.insert(9);
+
+        assertEquals(List.of(0, 1, 3, 4, 5, 6, 7, 8, 9, 11, 12), numbers.getElements());
+
+        removeAndTest(numbers, "remove/case3_input.json", "remove/case3_output.json", 6, 10);
+    }
+
+    private void testRemovingNonRootNodeWithTwoChildrenAndSuccessorIsImmediateRightAndSuccessorIsHavingOneChild() throws NoSuchFieldException, IllegalAccessException, IOException {
+        BinarySearchTree<Integer> numbers = new BinarySearchTree<>();
+
+        numbers.insert(12);
+        numbers.insert(5);
+        numbers.insert(3);
+        numbers.insert(7);
+        numbers.insert(2);
+        numbers.insert(1);
+        numbers.insert(9);
+        numbers.insert(8);
+        numbers.insert(11);
+        numbers.insert(14);
+        numbers.insert(13);
+        numbers.insert(17);
+        numbers.insert(20);
+        numbers.insert(18);
+
+        assertEquals(14, numbers.size());
+        assertEquals(List.of(1, 2, 3, 5, 7, 8, 9, 11, 12, 13, 14, 17, 18, 20), numbers.getElements());
+
+        removeAndTest(numbers, "remove/case2_input.json", "remove/case2_output.json", 14, 13);
+    }
+
+    private void testRemovingRightParentWithTwoChildren(BinarySearchTree<String> stringTree) throws NoSuchFieldException, IOException, IllegalAccessException {
         stringTree.empty();
 
         stringTree.insert("E");
         stringTree.insert("G");
         stringTree.insert("F");
         stringTree.insert("H");
-        assertTrue(stringTree.remove("G"));
-        assertEquals(3, stringTree.size());
+        removeAndTest(stringTree, "remove/case12_input.json", "remove/case12_output.json", "G", 3);
         assertTrue(stringTree.remove("F"));
         assertEquals(2, stringTree.size());
         assertTrue(stringTree.remove("E"));
@@ -83,15 +181,14 @@ class BinarySearchTreeTest {
         assertEquals(0, stringTree.size());
     }
 
-    private static void testRemovingLeftParentWithTwoChildren(BinarySearchTree<String> stringTree) {
+    private void testRemovingLeftParentWithTwoChildren(BinarySearchTree<String> stringTree) throws NoSuchFieldException, IOException, IllegalAccessException {
         stringTree.empty();
 
         stringTree.insert("E");
         stringTree.insert("B");
         stringTree.insert("A");
         stringTree.insert("C");
-        assertTrue(stringTree.remove("B"));
-        assertEquals(3, stringTree.size());
+        removeAndTest(stringTree, "remove/case13_input.json", "remove/case13_output.json", "B", 3);
         assertTrue(stringTree.remove("A"));
         assertEquals(2, stringTree.size());
         assertTrue(stringTree.remove("C"));
@@ -100,104 +197,104 @@ class BinarySearchTreeTest {
         assertEquals(0, stringTree.size());
     }
 
-    private static void testRemovingRootWithTwoChildren(BinarySearchTree<String> stringTree) {
+    private void testRemovingRootWithTwoChildren(BinarySearchTree<String> stringTree) throws NoSuchFieldException, IOException, IllegalAccessException {
         stringTree.empty();
 
         stringTree.insert("B");
         stringTree.insert("A");
         stringTree.insert("C");
-        assertTrue(stringTree.remove("B"));
-        assertEquals(2, stringTree.size());
+        removeAndTest(stringTree, "remove/case11_input.json", "remove/case11_output.json", "B", 2);
         assertTrue(stringTree.remove("A"));
         assertEquals(1, stringTree.size());
         assertTrue(stringTree.remove("C"));
         assertEquals(0, stringTree.size());
     }
 
-    private static void testRemovingParentNodeWithOnlyOneChildOnTheRight(BinarySearchTree<String> stringTree) {
+    private void testRemovingParentNodeWithOnlyOneChildOnTheRight(BinarySearchTree<String> stringTree) throws NoSuchFieldException, IOException, IllegalAccessException {
         stringTree.empty();
 
         stringTree.insert("A");
         stringTree.insert("B");
         stringTree.insert("C");
-        assertTrue(stringTree.remove("B"));
-        assertEquals(2, stringTree.size());
+        removeAndTest(stringTree, "remove/case9_input.json", "remove/case9_output.json", "B", 2);
         assertTrue(stringTree.remove("C"));
         assertEquals(1, stringTree.size());
         assertTrue(stringTree.remove("A"));
         assertEquals(0, stringTree.size());
     }
 
-    private static void testRemovingParentNodeWithOnlyOneChildOnTheLeft(BinarySearchTree<String> stringTree) {
+    private void testRemovingParentNodeWithOnlyOneChildOnTheLeft(BinarySearchTree<String> stringTree) throws NoSuchFieldException, IOException, IllegalAccessException {
         stringTree.empty();
 
         stringTree.insert("C");
         stringTree.insert("B");
         stringTree.insert("A");
-        assertTrue(stringTree.remove("B"));
-        assertEquals(2, stringTree.size());
+        removeAndTest(stringTree, "remove/case10_input.json", "remove/case10_output.json", "B", 2);
         assertTrue(stringTree.remove("C"));
         assertEquals(1, stringTree.size());
         assertTrue(stringTree.remove("A"));
         assertEquals(0, stringTree.size());
     }
 
-    private static void testRemovingLeafNodeWhichIsOnTheLeftOfParent(BinarySearchTree<String> stringTree) {
+    private void testRemovingLeafNodeWhichIsOnTheLeftOfParent(BinarySearchTree<String> stringTree) throws NoSuchFieldException, IllegalAccessException, IOException {
         stringTree.empty();
 
         stringTree.insert("B");
         stringTree.insert("A");
         stringTree.insert("C");
-        assertTrue(stringTree.remove("A"));
-        assertEquals(2, stringTree.size());
+
+        removeAndTest(stringTree, "remove/case5_input.json", "remove/case5_output.json", "A", 2);
+
         assertTrue(stringTree.remove("B"));
         assertEquals(1, stringTree.size());
         assertTrue(stringTree.remove("C"));
         assertEquals(0, stringTree.size());
     }
 
-    private static void testRemovingLeafNodeWhichIsOnTheRightOfParent(BinarySearchTree<String> stringTree) {
+    private void testRemovingLeafNodeWhichIsOnTheRightOfParent(BinarySearchTree<String> stringTree) throws NoSuchFieldException, IllegalAccessException, IOException {
         stringTree.empty();
 
         stringTree.insert("B");
         stringTree.insert("A");
         stringTree.insert("C");
-        assertTrue(stringTree.remove("C"));
-        assertEquals(2, stringTree.size());
+        removeAndTest(stringTree, "remove/case6_input.json", "remove/case6_output.json", "C", 2);
         assertTrue(stringTree.remove("B"));
         assertEquals(1, stringTree.size());
         assertTrue(stringTree.remove("A"));
         assertEquals(0, stringTree.size());
     }
 
-    private static void testRemovingRootNodeWithOnlyOneChildOnTheRight(BinarySearchTree<String> stringTree) {
+    private void testRemovingRootNodeWithOnlyOneChildOnTheRight(BinarySearchTree<String> stringTree) throws NoSuchFieldException, IllegalAccessException, IOException {
         stringTree.empty();
 
         stringTree.insert("A");
         stringTree.insert("B");
-        assertTrue(stringTree.remove("A"));
-        assertEquals(1, stringTree.size());
+        removeAndTest(stringTree, "remove/case7_input.json", "remove/case7_output.json", "A", 1);
         assertTrue(stringTree.remove("B"));
         assertEquals(0, stringTree.size());
     }
 
-    private static void testRemovingRootNodeWithOnlyOneChildOnTheLeft(BinarySearchTree<String> stringTree) {
+    private void print(BinarySearchTree<String> stringTree) throws NoSuchFieldException, IllegalAccessException, JsonProcessingException {
+        BinarySearchTree.Node<String> rootNode = getRootNode(stringTree);
+        System.out.println(objectMapper.writeValueAsString(rootNode));
+    }
+
+    private void testRemovingRootNodeWithOnlyOneChildOnTheLeft(BinarySearchTree<String> stringTree) throws NoSuchFieldException, IOException, IllegalAccessException {
         stringTree.empty();
 
         stringTree.insert("B");
         stringTree.insert("A");
-        assertTrue(stringTree.remove("B"));
-        assertEquals(1, stringTree.size());
+        removeAndTest(stringTree, "remove/case8_input.json", "remove/case8_output.json", "B", 1);
         assertTrue(stringTree.remove("A"));
         assertEquals(0, stringTree.size());
     }
 
-    private static void testRemovingOnlyElementInTheTree(BinarySearchTree<String> stringTree) {
+    private void testRemovingOnlyElementInTheTree(BinarySearchTree<String> stringTree) throws IOException, NoSuchFieldException, IllegalAccessException {
         stringTree.empty();
 
         stringTree.insert("A");
-        assertTrue(stringTree.remove("A"));
-        assertEquals(0, stringTree.size());
+
+        removeAndTest(stringTree, "remove/case4_input.json", null, "A", 0);
     }
 
     private static void testEmptyTree(BinarySearchTree<String> stringTree) {
@@ -264,5 +361,11 @@ class BinarySearchTreeTest {
         assertEquals(3, elements.get(3));
         assertEquals(4, elements.get(4));
         assertEquals(10, elements.get(10));
+    }
+
+    private static <E extends Comparable<E>> BinarySearchTree.Node<E> getRootNode(BinarySearchTree<E> numbers) throws NoSuchFieldException, IllegalAccessException {
+        Field elementsField = numbers.getClass().getDeclaredField("root");
+        elementsField.setAccessible(true);
+        return (BinarySearchTree.Node<E>) elementsField.get(numbers);
     }
 }
