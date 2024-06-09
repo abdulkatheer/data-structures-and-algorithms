6672383a8d379fe1b1b17354c4fec5c2a3901516
Mohideen Abdul Katheer M <abdulkatheerm@gmail.com>
2024-06-09 20:37:21 +0530

    Insertion sort

---
+++ algorithms/sort/src/main/java/io/abdul/InsertionSort.java
@@ -0,0 +1,45 @@
+package io.abdul;
+
+import io.abdul.api.exception.NotImplemented;
+
+/**
+ * Time Complexity: <br>
+ * O(n<sup>2</sup>) - When elements are random <br>
+ * \u03A9(n) - When elements are already sorted <br>
+ * Space Complexity - O(1)
+ * <p>
+ * Once all elements are sorted, no swaps will happen.
+ * Idea is to sense that no swaps didn't happen in previous iteration and stop further iterations
+ *
+ * @param <E>
+ */
+public class InsertionSort<E extends Comparable<E>> implements Sort<E> {
+    @Override
+    public void sortInPlace(E[] elements) {
+        if (elements.length <= 1) {
+            return;
+        }
+        for (int i = 1; i < elements.length; i++) {
+            int position = i;
+            E toBeInserted = elements[i];
+            for (int j = i - 1; j >= 0; j--) {
+                // 1 3 4 2
+                // 1 3 4 4
+                // 1 3 3 4
+                if (elements[j].compareTo(toBeInserted) <= 0) {
+                    break;
+                }
+                elements[j + 1] = elements[j]; // making room for insertion
+                position = j;
+            }
+            if (position != i) {
+                elements[position] = toBeInserted; //  insert in the space made
+            }
+        }
+    }
+
+    @Override
+    public E[] sort(E[] elements) {
+        throw new NotImplemented("sort() not implemented in " + BubbleSort.class.getName());
+    }
+}
